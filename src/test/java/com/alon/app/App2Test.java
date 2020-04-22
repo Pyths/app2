8e7c2ae911f8624d54ee8ca0f1ff3ccc38dc2e12
Pyths <jalon5747@gmail.com>
2020-04-21 20:34:56 -0500

    ADDING MAVING LAYOUT

---
+++ src/test/java/com/alon/app/App2Test.java
@@ -0,0 +1,47 @@
+package com.alon.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class App2Test 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+	public void test1() {
+		assertEquals("joel", "joel");
+	}
+	
+	@Test
+	public void test2() {
+		assertEquals("joel", "joel");
+	}
+	
+	@Test
+	public void test3() {
+		assertEquals("joel", "joel");
+	}
+	
+	@Test
+	public void test4() {
+		assertEquals("joel", "joel");
+	}
+	
+	@Test
+	public void test5() {
+		assertEquals("joel", "joel");
+	}
+
+}
